1eaac6b616ed12231014a8799fb738ddb5031869
Marvania-Shreya <smarvaina@gmail.com>
2025-06-17 12:51:25 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) we are using node.js , not browser
+
+let name = "shreya"//string
+let age = 19//number
+let isLoggedIn = false//boolean 
+let state = null //null
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value or null is representation of empty value
+// undefined => variable is declared but value is not defined 
+// symbol => used for uniqueness
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
