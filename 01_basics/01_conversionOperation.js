514773a0f4ffc2585bfc0eb2525ae4bdad07e572
Marvania-Shreya <smarvaina@gmail.com>
2025-06-17 17:17:46 +0530

    Update 01_conversionOperation.js

--- 01_basics/01_conversionOperation.js
+++ 01_basics/01_conversionOperation.js
@@ -42,38 +42,35 @@ let stringNumber = String(someNumber)
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+// console.log(negValue);//-3
 
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**3);
-// console.log(2/3);
-// console.log(2%3);
+// console.log(2+2);//4
+// console.log(2-2);//0
+// console.log(2*2);//4
+// console.log(2**3);//8
+// console.log(2/3);//0.6666666666666666
+// console.log(2%3);//2
 
 let str1 = "hello"
-let str2 = " hitesh"
+let str2 = " shreya "
 
 let str3 = str1 + str2
-// console.log(str3);
+// console.log(str3); // hello shreya
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2);// 12
+// console.log(1 + "2");// 12
+// console.log("1" + 2 + 2);// 122
+// console.log(1 + 2 + "2");// 122
 
-// console.log( (3 + 4) * 5 % 3);
+// console.log( (3 + 4) * 5 % 3);// 2
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true);// 1
+// console.log(+"");// 0
 
 let num1, num2, num3
-
 num1 = num2 = num3 = 2 + 2
+//console.log(num3);//4
 
 let gameCounter = 100
 ++gameCounter;
-console.log(gameCounter);
-
-// link to study
-// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+// console.log(gameCounter);//101
