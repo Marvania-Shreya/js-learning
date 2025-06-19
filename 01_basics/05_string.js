055b7a77808aaa2079581146a8f9eeacc7903060
Marvania-Shreya <smarvaina@gmail.com>
2025-06-19 13:44:47 +0000

    string and it's method

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -31,3 +31,11 @@ const url = "https://hitesh.com/shreya%20marvania"
 console.log(url.replace('%20', '-'))//https://marvania.com/shreya-marvania
 console.log(url.includes('sundar'))//false
 console.log(gameName.split('-'));//[ 'hitesh', 'hc', 'com' ]
+
+
+// String.prototype.at() => The at() method of String values takes an integer value and returns a new String => Syntax:at(index)
+
+// String.prototype.charAt() => The charAt() method of String values returns a new string consisting of the single UTF-16 code unit at the given index. => Syntax:charAt(index)
+
+// String.prototype.indexOf() => The indexOf() method of String values searches this string and returns the index of the first occurrence of the specified substring. => Syntax:indexOf(searchString)
+//indexOf(searchString, position)
