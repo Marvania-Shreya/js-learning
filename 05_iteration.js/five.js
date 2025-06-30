f41a9b4262f00271fa6b524724b01c701bdca950
Marvania-Shreya <smarvaina@gmail.com>
2025-06-30 10:27:14 +0000

    forEach

--- 05_iteration.js/five.js
+++ 05_iteration.js/five.js
@@ -13,7 +13,6 @@ const coding = ["js", "ruby", "java", "python", "cpp"]
 // }
 
 // coding.forEach(printMe)
-
 // coding.forEach( (item, index, arr)=> {
 //     console.log(item, index, arr);
 // } )
